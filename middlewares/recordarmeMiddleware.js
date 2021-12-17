25ea97cf274c84cbd43a944945fc5c2726a73daa
Yoshemith <cv3227@innovaccion.mx>
2021-12-16 23:42:42 -0600

    Back user data fixed!

--- middlewares/recordarmeMiddleware.js
+++ middlewares/recordarmeMiddleware.js
@@ -1,32 +1,30 @@
 const db = require('../database/models');
 const sequelize = db.sequelize;
-
 //Llamar a los modelos
 const User = db.User;
 
-//AQUI REORGANIZAR PARA GAMINGO_DB
-function recordarmeMiddleware(req, res, next){
+async function recordarmeMiddleware(req, res, next){
     res.locals.isLogged = false;
-    
+
     let emailInCookie = req.cookies.recordar; //checamos las cookies
-    let userFromCookie;
     if(emailInCookie != null || emailInCookie != undefined){
-        User.findOne({ raw: true, where: { email: emailInCookie }}).then(user => {
-            userFromCookie = user;
+        //pedido asincronico a la db y seteo con promesa
+        await User.findOne({ raw: true, where: { email: emailInCookie }}).then(user => {
             //Si esta en la BD entonces se loguea on session
-            if (userFromCookie != null || userFromCookie != undefined){
-                req.session.usuarioLogeado = userFromCookie;
+            if (user != null || user != undefined){
+                req.session.usuarioLogeado = user;
+                //res.locals.isLogged = true;
+                //res.locals.userLogged = req.session.usuarioLogeado;
             }
-            
-        });
+        }).catch(err => {
+            console.log(err);
+       });
     }
-    
     //si esta logueado en session entonces se manda a locals
     if (req.session && req.session.usuarioLogeado){
         res.locals.isLogged = true;
         res.locals.userLogged = req.session.usuarioLogeado;
-    }
-
+    } 
     next();
 }
 
